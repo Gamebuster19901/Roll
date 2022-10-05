e3917b3c9f9f4d7227306183058896de279dac5c
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-10-05 00:17:04 -0400

    Character command

---
+++ src/main/java/com/gamebuster19901/excite/bot/command/CharacterCommand.java
@@ -0,0 +1,11 @@
+package com.gamebuster19901.excite.bot.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+
+public class CharacterCommand {
+
+	public static void register(CommandDispatcher<CommandContext> dispatcher) {
+		
+	}
+	
+}
