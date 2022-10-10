1a399dcb9688f5c1106f3663b77b596e21a7b639
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-10-10 17:18:04 -0400

    PlayerCharacterArgument no longer requires "https://"

--- src/main/java/com/gamebuster19901/excite/bot/command/argument/PlayerCharacterArgument.java
+++ src/main/java/com/gamebuster19901/excite/bot/command/argument/PlayerCharacterArgument.java
@@ -1,7 +1,7 @@
 package com.gamebuster19901.excite.bot.command.argument;
 
 import com.gamebuster19901.excite.bot.command.Commands;
-import com.gamebuster19901.excite.bot.game.character.DNDBeyondPDFPlayerBuilder;
+import com.gamebuster19901.excite.bot.game.beyond.character.DNDBeyondPDFPlayerBuilder;
 import com.gamebuster19901.excite.bot.game.character.PlayerCharacter;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
@@ -15,6 +15,9 @@ public class PlayerCharacterArgument implements ArgumentType<PlayerCharacter>{
 	@Override
 	public <S> PlayerCharacter parse(S context, StringReader reader) throws CommandSyntaxException {
 		String character = Commands.readString(reader);
+		if(!character.startsWith("https://")) {
+			character = "https://" + character;
+		}
 		if(character.startsWith(DND_BEYOND_PDF) && character.endsWith(PDF)) {
 			return new DNDBeyondPDFPlayerBuilder(character).build();
 		}
