f45dff95c8836d00ded2c060aa6f82a5af98680e
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-11-17 00:16:26 -0500

    Add an exception for when something is not found when evaluating an argument

--- src/main/java/com/gamebuster19901/roll/bot/command/exception/ParseExceptions.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/exception/ParseExceptions.java
@@ -1,6 +1,7 @@
 package com.gamebuster19901.roll.bot.command.exception;
 
 import com.mojang.brigadier.LiteralMessage;
+import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 
 public interface ParseExceptions {
@@ -8,4 +9,5 @@ public interface ParseExceptions {
 	public DynamicCommandExceptionType INVALID_PDF_DOMAIN = new DynamicCommandExceptionType(url -> new LiteralMessage("`" + url + "` is not a PDF I know how to parse"));
 	public Dynamic2CommandExceptionWithCause UNABLE_TO_PARSE_PDF_VALUE = new Dynamic2CommandExceptionWithCause((name, value, reason) -> new LiteralMessage("Unable to parse `" + name + "` field from PDF.\n\nUnparsable value: `" + value + "`\n\nReason: " + reason.getClass().getCanonicalName() + " - "+ reason.getMessage()));
 	
+	public Dynamic2CommandExceptionType NONEXISTANT = new Dynamic2CommandExceptionType((type, value) -> new LiteralMessage(type + " `" + value + "` does not exist or could not be found."));
 }
