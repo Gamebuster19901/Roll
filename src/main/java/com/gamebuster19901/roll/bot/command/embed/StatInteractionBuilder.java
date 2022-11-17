48a99912ce64612154d8d09172e0587e5866c5a9
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-11-17 01:12:43 -0500

    Remember the state of the page selection row when turning pages

--- src/main/java/com/gamebuster19901/roll/bot/command/embed/StatInteractionBuilder.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/embed/StatInteractionBuilder.java
@@ -9,6 +9,7 @@ import com.gamebuster19901.roll.bot.game.Statted;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.ItemComponent;
+import net.dv8tion.jda.api.interactions.components.LayoutComponent;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu.Builder;
@@ -30,11 +31,15 @@ public class StatInteractionBuilder {
 	}
 	
 	public StatInteractionBuilder(Statted statted, Message message) {
-		this(statted, StatEmbedPage.valueOf(message.getEmbeds().get(0).getTitle()), isShowingMenu(message));
+		this(statted, getPageFromMessage(statted, message), isShowingMenu(message));
+	}
+	
+	public StatInteractionBuilder(Statted statted, Message message, StatEmbedPage newPage) {
+		this(statted, newPage, isShowingMenu(message));
 	}
 	
 	public StatInteractionBuilder(Statted statted, Message message, boolean showMenu) {
-		this(statted, StatEmbedPage.valueOf(message.getEmbeds().get(0).getTitle()), showMenu);
+		this(statted, getPageFromMessage(statted, message), showMenu);
 	}
 	
 	public StatInteractionBuilder(Statted statted, StatEmbedPage page, boolean showMenu) {
@@ -57,7 +62,7 @@ public class StatInteractionBuilder {
 				components.add(Button.secondary("charsheet page " + statted.getID() + " " + prevPage.name(), prevPage.name()));
 			}
 			if(menu != null) {
-				components.add(Button.secondary("charsheet menu show " + statted.getID() + " " + page.name(), MENU));
+				components.add(Button.primary("charsheet menu show " + statted.getID() + " " + page.name(), MENU));
 			}
 			if(nextPage != null) {
 				components.add(Button.secondary("charsheet page " + statted.getID() + " " + nextPage.name(), nextPage.name()));
@@ -86,7 +91,9 @@ public class StatInteractionBuilder {
 	
 	private StringSelectMenu getSelectMenu(List<StatEmbedPage> validPages) {
 		Builder menuBuilder = StringSelectMenu.create("charsheet");
+		menuBuilder.setPlaceholder("Select a page");
 
+		menuBuilder.addOption("Return to normal view", "menu hide " + statted.getID() + " " + page.name());
 		for(StatEmbedPage page : validPages) {
 			if(page != this.page) {
 				menuBuilder.addOption(page.name(), "page " + statted.getID() + " " + page.name());
@@ -138,16 +145,41 @@ public class StatInteractionBuilder {
 	}
 	
 	private static boolean isShowingMenu(Message message) {
-		if (message.getButtons().size() > 2) {
-			for(Button b : message.getButtons()) {
-				if(b.getEmoji() != null) {
-					if(message.getButtons().get(1).getEmoji().getFormatted().equals(MENU)) {
-						return true;
+		for(LayoutComponent c : message.getActionRows()) {
+			if(c instanceof ActionRow) {
+				for(ItemComponent i : c.getActionComponents()) {
+					if(i instanceof StringSelectMenu) {
+						StringSelectMenu menu = (StringSelectMenu) i;
+						if(menu.getId().equals("charsheet")) {
+							return true;
+						}
 					}
 				}
 			}
 		}
 		return false;
+	}
+	
+	/**
+	 * @param statted the creature to see stats of
+	 * @param message the message to get the page of
+	 * @return the page of the provided message, or {@link StatEmbedPage.Stats} if the page is invalid or nonexistant
+	 */
+	private static StatEmbedPage getPageFromMessage(Statted statted, Message message) {
+		if(message.getEmbeds().size() > 0) { 
+			try {
+				StatEmbedPage page = StatEmbedPage.valueOf(message.getEmbeds().get(0).getTitle());
+				if(getValidPages(statted).contains(page)) {
+					return page;
+				}
+			}
+			catch(IllegalArgumentException e) {
+				e.printStackTrace();
+				//Swallow, if title is somehow not a known page, just show the stats to the user
+				//This should never happen so it is exceptional
+			}
+		}
+		return StatEmbedPage.Stats;
 	}
 	
 }
--- src/main/java/com/gamebuster19901/roll/bot/command/interaction/CharacterSheetInteraction.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/interaction/CharacterSheetInteraction.java
@@ -11,6 +11,7 @@ import com.gamebuster19901.roll.bot.game.Statted;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.interactions.components.ComponentInteraction;
 import net.dv8tion.jda.api.requests.restaction.interactions.MessageEditCallbackAction;
 import net.dv8tion.jda.api.utils.messages.MessageEditBuilder;
@@ -25,10 +26,11 @@ public class CharacterSheetInteraction {
 						.executes((context) -> {
 							CommandContext<?> c = context.getSource();
 							ComponentInteraction e = c.getEvent(ComponentInteraction.class);
+							Message oldMessage = e.getMessage();
 							MessageEditBuilder editBuilder = new MessageEditBuilder();
 							Statted statted = context.getArgument("statted", Statted.class);
 							MessageEditCallbackAction editAction = e.deferEdit();
-							switchPage(e, editAction, editBuilder, statted, context.getArgument("page", StatEmbedPage.class));
+							switchPage(e, editAction, editBuilder, statted, oldMessage, context.getArgument("page", StatEmbedPage.class));
 							editAction.queue();
 							return 1;
 						})
@@ -57,8 +59,8 @@ public class CharacterSheetInteraction {
 	}
 	
 	
-	private static MessageEditCallbackAction switchPage(ComponentInteraction i, MessageEditCallbackAction e, MessageEditBuilder b, Statted statted, StatEmbedPage page) {
-		return e.setEmbeds(new StatEmbedBuilder(statted, page, b).getEmbed()).setComponents(new StatInteractionBuilder(statted, page).getComponents());
+	private static MessageEditCallbackAction switchPage(ComponentInteraction i, MessageEditCallbackAction e, MessageEditBuilder b, Statted statted, Message message, StatEmbedPage page) {
+		return e.setEmbeds(new StatEmbedBuilder(statted, page, b).getEmbed()).setComponents(new StatInteractionBuilder(statted, message, page).getComponents());
 	}
 	
 	public static MessageEditCallbackAction showDropdown(ComponentInteraction i, MessageEditCallbackAction e, MessageEditBuilder b, Statted statted, boolean showMenu) {
