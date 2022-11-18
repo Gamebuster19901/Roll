be9412528e109584d5ec2d019b8cf80b6143ffcc
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-11-17 22:11:05 -0500

    a

--- src/main/java/com/gamebuster19901/roll/bot/command/RollCommand.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/RollCommand.java
@@ -50,8 +50,9 @@ class RollCommand {
 								.setFooter("Result: " + result + ". Min: " + roll.getMinValue() + " Max: " + roll.getMaxValue())
 								.build());
 						if(roll.isSortable()) {
-							action.addActionRow(Button.secondary("Probability distribution", Emoji.fromUnicode("U+1F4C8")));
+							//action.addActionRow(Button.primary("Sort", "Sort Dice"), Button.secondary("Probability distribution", Emoji.fromUnicode("U+1F4C8")));
 						}
+						action.addActionRow(Button.secondary("Probability distribution", Emoji.fromUnicode("U+1F4C8")));
 						action.queue();
 							//e.reply("Rolling " + dice).addFiles(FileUpload.fromData(in, "test.png")).complete();
 
---
+++ src/main/java/com/gamebuster19901/roll/bot/command/argument/CommonRollArgumentType.java
@@ -0,0 +1,9 @@
+package com.gamebuster19901.roll.bot.command.argument;
+
+public class CommonRollArgumentType {
+
+	public static enum CommonRollTypes {
+		
+	}
+	
+}
