ad0b00651f70756bfaada35e969bf8679fb870d6
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2022-11-25 01:09:22 -0500

    Add skills to /check command

--- src/main/java/com/gamebuster19901/roll/bot/command/CheckCommand.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/CheckCommand.java
@@ -1,10 +1,12 @@
 package com.gamebuster19901.roll.bot.command;
 
 import com.gamebuster19901.roll.bot.command.argument.AbilityArgumentType;
+import com.gamebuster19901.roll.bot.command.argument.SkillArgumentType;
 import com.gamebuster19901.roll.bot.game.Dice;
 import com.gamebuster19901.roll.bot.game.Statted;
 import com.gamebuster19901.roll.bot.game.character.PlayerCharacter;
 import com.gamebuster19901.roll.bot.game.stat.Ability;
+import com.gamebuster19901.roll.bot.game.stat.Skill;
 import com.mojang.brigadier.CommandDispatcher;
 
 import net.dv8tion.jda.api.interactions.callbacks.IReplyCallback;
@@ -17,6 +19,11 @@ class CheckCommand {
 			.then(Commands.argument("ability", AbilityArgumentType.ANY_ABILITY)
 				.executes((context) -> {
 					return rollCheck(context.getSource(), context.getArgument("ability", Ability.class));
+				})
+			)
+			.then(Commands.argument("skill", SkillArgumentType.ANY_SKILL)
+				.executes((context) -> {
+					return rollSkill(context.getSource(), context.getArgument("skill", Skill.class));
 				})
 			)
 		);
@@ -33,5 +40,13 @@ class CheckCommand {
 		return 1;
 	}
 	
+	private static int rollSkill(CommandContext<IReplyCallback> c, Skill skill) {
+		if(PlayerCharacter.hasActiveCharacter(c.getAuthor())) {
+			Statted character = PlayerCharacter.getActiveCharacter(c.getAuthor());
+			Dice dice = new Dice("d20+" + skill.getName());
+			RollCommand.roll(c, skill.getName() + " check", dice, character);
+		}
+		return 1;
+	}
 	
 }
