883175fcedd3c40725e6d915b8b464ba90a3f7ce
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2023-05-02 23:10:35 -0400

    Use suggestions properly in DiceArgumentType

--- src/main/java/com/gamebuster19901/roll/bot/command/argument/DiceArgumentType.java
+++ src/main/java/com/gamebuster19901/roll/bot/command/argument/DiceArgumentType.java
@@ -1,8 +1,8 @@
 package com.gamebuster19901.roll.bot.command.argument;
 
 import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,6 +16,7 @@ import com.gamebuster19901.roll.bot.game.character.Stat;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.context.StringRange;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestion;
 import com.mojang.brigadier.suggestion.Suggestions;
@@ -24,6 +25,7 @@ import com.mysql.cj.util.StringUtils;
 
 public class DiceArgumentType implements ArgumentType<Dice> {
 	
+	public static final Pattern DELIMITER_REGEX = Pattern.compile("(.*?)([+\\-])");
 	public static final Pattern DICE_REGEX = Pattern.compile("^(?<amount>\\d*)d(?<die>\\d*)$");
 	
 	private LinkedHashSet<String> suggestions = new LinkedHashSet<String>();
@@ -64,145 +66,173 @@ public class DiceArgumentType implements ArgumentType<Dice> {
 	public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
 		String input = Commands.lastArgOf(builder.getInput());
 		String lowercaseInput = Commands.lastArgOf(builder.getInput().toLowerCase());
-		if(builder.getInput().isBlank()) {
-			builder.suggest("<Dice>");
-			return builder.buildFuture();
-		}
 		
 		if(input.isBlank()) {
-			builder.suggest("<Dice>");
+			builder.suggest(new Suggestion(new StringRange(0, 0), "<Dice>"));
+		}
+		
+		String lastValidArg = getLastValidArg(input);
+		StringRange lastValidRange = getLastArgRange(input);
+		String currentArg = getCurrentArg(input);
+		StringRange currentRange = getCurrentArgRange(input);
+		boolean isDelimiter = isDelimiter(currentArg);
+		
+		int lastArgEnd = getLastArgRange(input).getEnd();
+		StringRange replaceRange = 
+				isDelimiter 
+					? new StringRange(input.length(), input.length())
+					: currentRange;
+		
+		if(isDelimiter || input.isBlank()) {
+			builder.suggest(new Suggestion(replaceRange, input + "<Dice>"));
+			for(String suggestion : suggestions) {
+				builder.suggest(new Suggestion(replaceRange, input + suggestion));
+			}
 			return builder.buildFuture();
 		}
 		
-		String lastArg;
-		int index = Math.max(input.lastIndexOf('+') + 1, input.lastIndexOf('-') + 1);
-		System.err.println("Index: " + index);
-		System.err.println("Length: " + input.length());
-		if(index == 0) {
-			lastArg = input.substring(0, input.length());
-		}
-		else {
-			lastArg = input.substring(index, input.length());
+		for(String suggestion : suggestions) {
+			if(suggestion.toLowerCase().startsWith(currentArg.toLowerCase())) {
+				if(suggestion.equalsIgnoreCase(currentArg)) {
+					StringRange range = replaceRange;
+					replaceRange = new StringRange(input.length(), input.length());
+					builder.suggest(new Suggestion(replaceRange, "+"));
+					builder.suggest(new Suggestion(replaceRange, "-"));
+					replaceRange = range;
+				}
+				else {
+					builder.suggest(new Suggestion(replaceRange, suggestion));
+				}
+			}
 		}
 		
-		System.err.println("Input: " + input);
-		System.err.println("Last Arg: " + lastArg);
-		if(!input.endsWith("+") && !input.endsWith("-")) {
-			for(String suggestion : suggestions) {
-				if(suggestion.startsWith(lastArg)) {
-					System.out.println("Suggestion: " + suggestion);
-					if(suggestion.equalsIgnoreCase(lastArg)) {
-						builder.forceSuggest(input + "+");
-						builder.forceSuggest(input + "-");
+		if(StringUtils.isStrictlyNumeric(currentArg)) {
+			StringRange old = replaceRange;
+			replaceRange = new StringRange(lastValidRange.getEnd(), input.length());
+			for(DieType type : DieType.getStandardDiceTypes()) {
+				builder.suggest(new Suggestion(replaceRange, lastValidArg + type.toString()));
+			}
+			replaceRange = old;
+		}
+		else if(currentArg.equalsIgnoreCase("d")) {
+			for(DieType type : DieType.getStandardDiceTypes()) {
+				builder.suggest(new Suggestion(replaceRange, type.toString()));
+			}
+		}
+		else {
+			Matcher matcher = DICE_REGEX.matcher(currentArg);
+			if(matcher.matches()) {
+				boolean foundSuggestion = false;
+				String amount = matcher.group("amount");
+				String die = matcher.group("die");
+				if(!amount.isBlank() && die.isBlank()) {
+					for(DieType type : DieType.getStandardDiceTypes()) {
+						builder.suggest(new Suggestion(replaceRange, amount + "d" + type.getSides() + ""));
+						foundSuggestion = true;
 					}
-					else {
-						builder.forceSuggest(input.substring(0, input.indexOf(lastArg)) + suggestion);
-					}
 				}
-			}
-			
-			if(StringUtils.isStrictlyNumeric(lastArg)) {
-				for(DieType type : DieType.getStandardDiceTypes()) {
-					builder.forceSuggest(input + type);
-				}
-			}
-			else if (lastArg.equalsIgnoreCase("d")) {
-				for(DieType type : DieType.getStandardDiceTypes()) {
-					builder.forceSuggest(input + type.getSides());
-				}
-			}
-			else {
-				Matcher matcher = DICE_REGEX.matcher(lastArg);
-				if(matcher.matches()) {
-					boolean foundSuggestion = false;
-					String amount = matcher.group("amount");
-					String die = matcher.group("die");
-					if(!amount.isBlank() && die.isBlank()) {
-						for(DieType type : DieType.getStandardDiceTypes()) {
-							builder.forceSuggest(input + type.getSides());
+				else if (amount.isBlank() && !die.isBlank()) {
+					for(DieType type : DieType.getStandardDiceTypes()) {
+						if((type.getSides() + "").startsWith(die + "")) {
+							builder.suggest(new Suggestion(replaceRange, "d" + type.getSides()));
 							foundSuggestion = true;
 						}
 					}
-					else if (amount.isBlank() && !die.isBlank()) {
-						for(DieType type : DieType.getStandardDiceTypes()) {
-							if((type.getSides() + "").startsWith(die + "")) {
-								builder.forceSuggest(input.substring(0, input.indexOf(lastArg)) + "d" + type.getSides());
-								foundSuggestion = true;
-							}
-						}
-					}
-					else if(!amount.isBlank() && !die.isBlank()) {
-						for(DieType type : DieType.getStandardDiceTypes()) {
-							if((type.getSides() + "").startsWith(die + "")) {
-								System.err.println("Suggested " + type);
-								builder.forceSuggest(input.substring(0, input.indexOf(lastArg)) + amount + "d" + type.getSides());
-								foundSuggestion = true;
-							}
-						}
-					}
-					else {
-						throw new AssertionError();
-					}
-					
-					if(!foundSuggestion) {
-						builder.forceSuggest(input);
-						builder.forceSuggest(input + "+");
-						builder.forceSuggest(input + "-");
-					}
 				}
-			}
-			
-			List<Suggestion> suggestions = builder.build().getList();
-			if(suggestions.size() == 1) {
-				if(suggestions.get(0).getText().equalsIgnoreCase(input)) {
-					builder.forceSuggest(input + "+");
-					builder.forceSuggest(input + "-");
+				else if(!amount.isBlank() && !die.isBlank()) {
+					for(DieType type : DieType.getStandardDiceTypes()) {
+						if((type.getSides() + "").startsWith(die + "")) {
+							System.err.println("Suggested " + type);
+							builder.suggest(new Suggestion(replaceRange, amount + "d" + type.getSides()));
+							if(currentArg.endsWith("d" + type.getSides())) {
+								builder.suggest(new Suggestion(replaceRange, amount + "d" + type.getSides() + "+"));
+								builder.suggest(new Suggestion(replaceRange, amount + "d" + type.getSides() + "-"));
+							}
+							foundSuggestion = true;
+						}
+					}
 				}
 				else {
-					System.err.println(suggestions.get(0).getText() + " != " + input);
+					throw new AssertionError();
 				}
-			}
-			
-		}
-		else {
-			builder.suggest(input + "<Dice>");
-			for(String suggestion : suggestions) {
-				builder.forceSuggest(input + suggestion);
-			}
-		}
-		
-		/*if(input.endsWith("-") || input.endsWith("+")) {
-			foundSuggestion = true;
-			builder.suggest(input + "<Dice>");
-			for(String suggestion : suggestions) {
-				builder.suggest(input + suggestion);
-			}
-		}
-		
-		if(!foundSuggestion) {
-			for(String suggestion : suggestions) {
-				if(input.endsWith(suggestion)) {
-					builder.suggest(input + "+");
-					builder.suggest(input + "-");
-					foundSuggestion = true;
+				
+				if(!foundSuggestion) {
+					builder.suggest(new Suggestion(replaceRange, input));
+					builder.suggest(new Suggestion(replaceRange, input + "+"));
+					builder.suggest(new Suggestion(replaceRange, input + "-"));
 				}
 			}
 		}
-		if(!foundSuggestion && !input.endsWith("d")) {
-			builder.suggest(input + "+");
-			builder.suggest(input + "-");
-		}
-		else if (!foundSuggestion && input.endsWith("d")) {
-			for(DieType type : DieType.getStandardDiceTypes()) {
-				builder.suggest(input.substring(0, input.length() - 1) + type.name());
-			}
-		}*/
 		
 		return builder.buildFuture();
 	}
 	
-	private void suggestDice(String input, String lastArg, SuggestionsBuilder suggestionsBuilder) {
+	/**
+	 * @param input the full command input
+	 * @return the StringRange representing the start and end of the argument
+	 */
+	private StringRange getLastArgRange(String input) {
 		
+		int lastSeparator = Math.max(input.indexOf('-'), input.indexOf('+'));
+		if(lastSeparator == -1) { 
+			return new StringRange(0, input.length());
+		}
+		else {
+			Matcher matcher = DELIMITER_REGEX.matcher(input);
+			MatchResult start = null;
+			MatchResult end = null;
+			
+			MatchResult result = null;
+			while(matcher.find()) {
+				result = matcher.toMatchResult();
+			}
+			
+			return new StringRange(result.start(), result.end());
+		}
+	}
+	
+	/**
+	 * Gets the last fully complete and valid subargument that the user typed.
+	 * 
+	 * This argument may or may not be the at the end of the input.
+	 * @param input
+	 * @return
+	 */
+	private String getLastValidArg(String input) {
+		if(input.isBlank()) {
+			return input;
+		}
+		StringRange range = getLastArgRange(input);
+		return input.substring(range.getStart(), range.getEnd());
+	}
+	
+
+	private StringRange getCurrentArgRange(String input) {
+		StringRange lastRange = getLastArgRange(input);
+		StringRange currentRange = new StringRange(lastRange.getEnd(), input.length());
+		if(lastRange.getEnd() == currentRange.getEnd()) {
+			return lastRange;
+		}
+		return currentRange;
+	}
+	
+	/**
+	 * Gets the last argument that the user typed.
+	 * 
+	 * This argument may not be valid, and is always at the end of the input.
+	 * @param input
+	 * @return
+	 */
+	private String getCurrentArg(String input) {
+		if(input.isBlank()) {
+			return input;
+		}
+		StringRange range = getCurrentArgRange(input);
+		return input.substring(range.getStart(), range.getEnd());
+	}
+	
+	private boolean isDelimiter(String s) {
+		return s.endsWith("+") || s.endsWith("-");
 	}
 	
 }
